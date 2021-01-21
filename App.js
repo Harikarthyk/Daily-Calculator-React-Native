87fcfdd352a6dace925e9ea505e0ec8f4c4ab080
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-01-21 07:53:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,114 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {View, Text} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Image Links
+// [yoga](https://user-images.githubusercontent.com/54505967/105271503-9a439900-5bbd-11eb-8b61-de913443e5ea.gif)
+// [friend](https://user-images.githubusercontent.com/54505967/105271506-9ca5f300-5bbd-11eb-8102-dbd7664166e6.gif)
+// [gym](https://user-images.githubusercontent.com/54505967/105271507-9d3e8980-5bbd-11eb-8743-6d7ca7cffeab.gif)
+// [other](https://user-images.githubusercontent.com/54505967/105271508-9dd72000-5bbd-11eb-915c-6e89d6d63715.gif)
+// [party](https://user-images.githubusercontent.com/54505967/105271510-9e6fb680-5bbd-11eb-95b3-a767a66e3575.gif)
+// [sport](https://user-images.githubusercontent.com/54505967/105271515-9fa0e380-5bbd-11eb-97e0-852afc166e0f.gif)
+// [study](https://user-images.githubusercontent.com/54505967/105271518-a0d21080-5bbd-11eb-85de-e75304732866.gif)
+// [welcome](https://user-images.githubusercontent.com/54505967/105271519-a16aa700-5bbd-11eb-8510-2722f4604dd5.gif)
+// [work](https://user-images.githubusercontent.com/54505967/105271525-a29bd400-5bbd-11eb-8999-64380116cf22.gif)
 
-const App: () => React$Node = () => {
+export const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+      <Text>Daily Calcultor App</Text>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
